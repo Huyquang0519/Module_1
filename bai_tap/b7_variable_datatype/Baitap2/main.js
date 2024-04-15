66abad8fed6aad1b4d211471edda52a53ce308e2
Huyquang0519 <huyquang0519@gmail.com>
2024-04-15 22:41:37 +0700

    update

--- bai_tap/b7_variable_datatype/Baitap2/main.js
+++ bai_tap/b7_variable_datatype/Baitap2/main.js
@@ -4,15 +4,15 @@ function convertCurrency() {
     let to = document.getElementById("to").value;
     let result;
 
-    if (from == "VND" && to == "USD"){
+    if (from === "VND" && to === "USD"){
         result = amount/23000 + " USD";
     }
-    else if (from == "USD" && to == "VND"){
+    else if (from === "USD" && to === "VND"){
         result = amount * 23000 + " VND";
     }
-    else if (from == to){
+    else if (from === to){
         result = amount;
-        if (to == "VND") result += " VND";
+        if (to === "VND") result += " VND";
         else result += " USD";
     }
     document.getElementById("result").innerHTML = result;
