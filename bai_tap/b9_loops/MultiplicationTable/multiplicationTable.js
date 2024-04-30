91b4b6bdb01cddbbd4018ea8a8904f78fb196fd7
Huyquang0519 <huyquang0519@gmail.com>
2024-04-30 15:55:19 +0700

    update_b9_loops

--- bai_tap/b9_loops/MultiplicationTable/multiplicationTable.js
+++ bai_tap/b9_loops/MultiplicationTable/multiplicationTable.js
@@ -1,6 +1,6 @@
 let sout;
 sout = "<table border='1' width='700px' >";
-for(let i = 1; i <= 10; i++){
+for(let i = 1; i < 10; i++){
     sout += "<tr>";
     for(let j = 2; j <= 9; j++){
         sout += "<td>" + j + "x" + i + "=" + j * i + "</td>";
@@ -9,3 +9,18 @@ for(let i = 1; i <= 10; i++){
 }
 sout += "</table>"
 document.getElementById("result").innerHTML = sout;
+
+
+let output = "", x = 1, y = 2;
+output += "<table border = '1' width='700px'>";
+while(x < 10){
+    output += "<tr>";
+    while(y <= 9){
+        output += "<td>" + x + " x " + y + " = " + x * y + "</td>";
+        y++;
+    }
+    output += "</tr>";
+    x++;
+}
+output += "</table>";
+document.getElementById("output").innerHTML = output;
