4ac5a32568514b092c917a285a7e905c11113f26
Huyquang0519 <huyquang0519@gmail.com>
2024-04-30 15:55:49 +0700

    b10_array

---
+++ thuc_hanh/b10_arrays/test.js
@@ -0,0 +1,17 @@
+// let numbers = [1, 2, 7, 4, 5];
+// let nums = [6, 7, 8];
+// let num3 = numbers.concat(nums);
+//
+// //compareFn: la 1 callback function trong js. Neu ham callback tra ve so lon hon 0 thi secondEl se dung truoc firstEl
+// // Neu ham callback tra ve so be hon hoac bang 0 thi thu tu duoc giu nguyen, tuc la firstEl se dung truoc secondEl
+// num3.sort(function (a, b){
+//     if(a >= b) return -1;
+//     else return 1;
+// });
+// console.log(num3);
+// var x = 3;
+// x = x + 2;
+// console.log(x+2);
+// console.log(x)
+
+
